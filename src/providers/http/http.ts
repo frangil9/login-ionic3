28ca8f99f488c388b7666bfc72abf581ae6fb59b
Developer <ing.franciscogil9@gmail.com>
2018-02-08 12:33:41 -0400

    cambios en url apuntando a heroku

--- src/providers/http/http.ts
+++ src/providers/http/http.ts
@@ -11,8 +11,7 @@ import { Injectable } from '@angular/core';
 export class HttpProvider {
 
   public pathRandomUser : string = 'https://randomuser.me/api/?results=25';
-  public pathAuth : string ='http://localhost:8080/login';
-  public pathSignup : string ='http://localhost:8080/signup';
+  public pathHeroku : string ='https://login-rest-api.herokuapp.com/';
   
   constructor(public http: HttpClient) {
     console.log('Hello HttpProvider Provider');
@@ -26,13 +25,13 @@ export class HttpProvider {
     let body = JSON.stringify(usuario);
     let headers = new HttpHeaders().set('Content-Type','application/json');
 
-    return this.http.post(this.pathAuth, body, {headers: headers});
+    return this.http.post(this.pathHeroku + 'login', body, {headers: headers});
   }
 
   signup(user){
     let body = JSON.stringify(user);
     let headers = new HttpHeaders().set('Content-Type','application/json');
 
-    return this.http.post(this.pathSignup, body, {headers: headers});
+    return this.http.post(this.pathHeroku + 'signup', body, {headers: headers});
   }
 }
